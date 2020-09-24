027ddad0800ac09a612495e145e84bee0cc8ed8f
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 15:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function shout(string) {
   return string = string.toUpperCase()
 }
+ function whisper(string) {
+   return string = string.toLowerCase()
+ }
+
+ function logShout(string) {
+   console.log(string.toUpperCase)
+ }
+
+ function logWhisper(string) {
+   console.lof(string.toLowerCase)
+ }
